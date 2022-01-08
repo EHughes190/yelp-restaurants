e38a9bed75f4ff6eea74b6ee969bccb37af421e6
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2022-01-08 23:34:41 +0000

    server setup, routes set up

---
+++ server/server.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurants: ["mcdonalds", "wendys", "pizza hut"],
+    },
+  });
+});
+
+//get a specific restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds",
+    },
+  });
+});
+
+//create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds",
+    },
+  });
+});
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "mcdonalds",
+    },
+  });
+});
+
+//delete a restaurant
+app.delete("api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
