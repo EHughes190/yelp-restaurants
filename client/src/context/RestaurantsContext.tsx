4d1fa29d2aee15938fdb1a36559a737518fef5c9
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2022-01-09 23:35:10 +0000

    add types to context

--- client/src/context/RestaurantsContext.tsx
+++ client/src/context/RestaurantsContext.tsx
@@ -1,13 +1,31 @@
 import React, { useState, createContext } from "react";
 
-export const RestaurantsContext = createContext({});
+type Restaurant = {
+  id: string;
+  name: string;
+  location: string;
+  price_range: number;
+};
 
-export const RestaurantsContextProvider = (props: any) => {
+type RestaurantContext = {
+  restaurants: Restaurant[];
+  setRestaurants: React.Dispatch<React.SetStateAction<never[]>>;
+};
+
+const defaultValue = {
+  restaurants: [],
+  setRestaurants: () => {},
+};
+
+export const RestaurantsContext =
+  createContext<RestaurantContext>(defaultValue);
+
+export const RestaurantsContextProvider: React.FC = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
 
   return (
     <RestaurantsContext.Provider value={{ restaurants, setRestaurants }}>
-      {props.children}
+      {children}
     </RestaurantsContext.Provider>
   );
 };
